95bafc436d8244d97566aeaae315124be53d70d9
Daisei Tanaka <d.tnk.4004@gmail.com>
2021-12-24 18:03:18 +0900

    commit test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import usersRoutes from './routes/users.js'
 
 const app = express();
 const PORT = 5000;
+//const PORT = 8080;
 
 app.use(bodyParser.json());
 
@@ -12,4 +13,6 @@ app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => res.send('Hello from Homepage.'));
 
-app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
+//app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
+//app.listen(PORT, () => console.log(`Server Running on port: http://172.31.41.169:${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on port: http://192.168.1.111:${PORT}`));
